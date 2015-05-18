12e879e6cf4b6d26fc7f78e7e527b06f89184c15
Karl Hobley <karlhobley10@gmail.com>
2015-05-18 21:11:36 +0100

    Add animation for top half of stickman

--- rippyshreddy.ts
+++ rippyshreddy.ts
@@ -162,6 +162,8 @@ class Stickman {
 
     private movePhase: number;
     private duckTransition: number;
+    private pitch: number;
+    private facingLeft: boolean;
 
     constructor(player: Player) {
         this.posX = 0;
@@ -171,6 +173,8 @@ class Stickman {
         this.player = player;
         this.movePhase = 0;
         this.duckTransition = 0;
+        this.pitch = 0;
+        this.facingLeft = false;
     }
 
     tick(dt: number) {
@@ -214,6 +218,40 @@ class Stickman {
             }
         }
 
+        // Targeting
+        const target = this.player.input.lookAt;
+        if (target) {
+            const duckTransition = this.duckTransition;
+            const neckHeight = 100 - 25 * duckTransition;
+            const hipHeight = 75 - 25 * duckTransition;
+
+            const positionX = this.posX;
+            const positionY = this.posY - neckHeight - hipHeight;
+            const targetX = target[0];
+            const targetY = target[1];
+
+            // Get difference between position and target
+            const diffX = targetX - positionX;
+            const diffY = targetY - positionY;
+
+            // Direction
+            this.facingLeft = diffX < 0;
+
+            // Find the distance
+            const distance = Math.sqrt(diffX * diffX + diffY * diffY);
+
+            // Work out the muzzle height
+            const muzzleHeight = 40;
+
+            // Find the angle (40 is the how low the gun is held below the neck)
+            this.pitch = Math.atan2(diffY, Math.abs(diffX)) - Math.asin(muzzleHeight / distance);
+
+            // Prevent NaN
+            if (!this.pitch) {
+                this.pitch = 0;
+            }
+        }
+
         // Move phase
         if (onFloor) {
             let legSpeed = this.velX * dt / 30;
@@ -243,6 +281,8 @@ class Stickman {
         const middlePositionX = (hipPositionX + neckPositionX) / 2;
         const middlePositionY = (hipPositionY + neckPositionY) / 2;
 
+        // Legs
+
         let leftFootPositionX = -20;
         let leftFootPositionY = 0;
         let rightFootPositionX = 20;
@@ -262,8 +302,6 @@ class Stickman {
             leftFootPositionY = Math.cos(-this.movePhase) * hipPositionY / 4 + (hipPositionY / 8) * running;
             rightFootPositionY = Math.cos(-this.movePhase + Math.PI) * hipPositionY / 4 + (hipPositionY / 8) * running;
         }
-
-        // Knees
 
         // Work out knee inversion
         const leftKneeInversion = moving ? -1: 1;
@@ -287,6 +325,60 @@ class Stickman {
             rightKneePositionX = -rightKneePositionX;
         }
 
+        // Arms
+
+        // Hands
+        let leftHandPosition = <Vector2>[30, 40];
+        let rightHandPosition = <Vector2>[30, 40];
+
+        // Elbows
+        let leftElbowPosition = calculateJoint(
+            [0, 0],
+            leftHandPosition,
+            55, 1);
+        let rightElbowPosition = calculateJoint(
+            [0, 0],
+            rightHandPosition,
+            55, 1);
+
+        let headPosition = <Vector2>[0, -30];
+
+        // Rotate
+        function rotate(vector: Vector2, a: number, b: number) {
+            const x = vector[0], y = vector[1];
+            vector[0] = x * b - y * a;
+            vector[1] = x * a + y * b;
+        }
+
+        var a = Math.sin(this.pitch);
+        var b = Math.cos(this.pitch);
+        rotate(leftHandPosition, a, b);
+        rotate(rightHandPosition, a, b);
+        rotate(leftElbowPosition, a, b);
+        rotate(rightElbowPosition, a, b);
+        rotate(headPosition, a, b);
+
+        // Invert if facing left
+        if (this.facingLeft) {
+            leftElbowPosition[0] = -leftElbowPosition[0];
+            rightElbowPosition[0] = -rightElbowPosition[0];
+            leftHandPosition[0] = -leftHandPosition[0];
+            rightHandPosition[0] = -rightHandPosition[0];
+            headPosition[0] = -headPosition[0];
+        }
+
+        // Translate
+        leftHandPosition[0] += neckPositionX;
+        leftHandPosition[1] += neckPositionY;
+        rightHandPosition[0] += neckPositionX;
+        rightHandPosition[1] += neckPositionY;
+        leftElbowPosition[0] += neckPositionX;
+        leftElbowPosition[1] += neckPositionY;
+        rightElbowPosition[0] += neckPositionX;
+        rightElbowPosition[1] += neckPositionY;
+        headPosition[0] += neckPositionX;
+        headPosition[1] += neckPositionY;
+
         context.save();
         context.translate(this.posX + this.velX * at, this.posY + this.velY * at);
 
@@ -303,10 +395,23 @@ class Stickman {
         context.lineTo(rightKneePositionX, rightKneePositionY);
         context.lineTo(hipPositionX, hipPositionY);
 
+        context.moveTo(leftHandPosition[0], leftHandPosition[1]);
+        context.lineTo(leftElbowPosition[0], leftElbowPosition[1]);
+        context.lineTo(neckPositionX, neckPositionY);
+
+        context.moveTo(rightHandPosition[0], rightHandPosition[1]);
+        context.lineTo(rightElbowPosition[0], rightElbowPosition[1]);
+        context.lineTo(neckPositionX, neckPositionY);
+
         context.moveTo(neckPositionX, neckPositionY);
         context.lineTo(hipPositionX, hipPositionY);
 
         context.stroke();
+
+        // Draw head
+        context.beginPath();
+        context.arc(headPosition[0], headPosition[1], 30, 0, Math.PI * 2);
+        context.fill();
 
         context.restore();
     }
