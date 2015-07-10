214765e59322ec4c1f0643abf3b40d8293510c4c
Karl Hobley <karlhobley10@gmail.com>
2015-07-10 18:29:32 +0100

    Particles now have a life span

--- fx/particles.ts
+++ fx/particles.ts
@@ -7,6 +7,7 @@ interface ParticleType {
     image: Assets.ImageAsset;
     forces: Vector2;
     damping: number;
+    maxLife: number;
 }
 
 interface Particle {
@@ -22,11 +23,13 @@ export class ParticleEngine {
             image: new Assets.ImageAsset('particles/smoke.png'),
             forces: [0, -30],
             damping: 0.6,
+            maxLife: 5,
         },
         blood: {
             image: new Assets.ImageAsset('particles/blood.png'),
             forces: [0, 1000],
             damping: 0.1,
+            maxLife: 5,
         }
     }
 
@@ -37,39 +40,52 @@ export class ParticleEngine {
             type: type,
             position: <Vector2>position.slice(),
             velocity: <Vector2>velocity.slice(),
-            life: 10
+            life: ParticleEngine.particleTypes[type].maxLife,
         });
     }
 
     tick(dt: number) {
         // Move particles
         for (const particle of this.particles) {
-            // Apply forces to velocity
-            const forces = ParticleEngine.particleTypes[particle.type].forces;
-            particle.velocity[0] += forces[0] * dt;
-            particle.velocity[1] += forces[1] * dt;
+            if (particle.life > 0) {
+                // Apply forces to velocity
+                const forces = ParticleEngine.particleTypes[particle.type].forces;
+                particle.velocity[0] += forces[0] * dt;
+                particle.velocity[1] += forces[1] * dt;
 
-            // Apply damping to velocity
-            function applyDamping(velocity: number, damping: number, dt: number) {
-                const force = (-velocity *  damping) ^ 0.5;
-                return velocity + force * dt;
+                // Apply damping to velocity
+                function applyDamping(velocity: number, damping: number, dt: number) {
+                    const force = (-velocity *  damping) ^ 0.5;
+                    return velocity + force * dt;
+                }
+                const damping = ParticleEngine.particleTypes[particle.type].damping;
+                particle.velocity[0] = applyDamping(particle.velocity[0], damping, dt);
+                particle.velocity[1] = applyDamping(particle.velocity[1], damping, dt);
+
+                // Apply velocity to position
+                particle.position[0] += particle.velocity[0] * dt;
+                particle.position[1] += particle.velocity[1] * dt;
+
+                // Update life
+                particle.life -= dt;
             }
-            const damping = ParticleEngine.particleTypes[particle.type].damping;
-            particle.velocity[0] = applyDamping(particle.velocity[0], damping, dt);
-            particle.velocity[1] = applyDamping(particle.velocity[1], damping, dt);
-
-            // Apply velocity to position
-            particle.position[0] += particle.velocity[0] * dt;
-            particle.position[1] += particle.velocity[1] * dt;
         }
     }
 
     draw(context: Context2D, at: number) {
+        context.save();
+
         // Draw particles
         for (const particle of this.particles) {
-            const image = ParticleEngine.particleTypes[particle.type].image.image;
-            context.drawImage(image, particle.position[0] - image.width / 2, particle.position[1] - image.height / 2);
+            if (particle.life > 0) {
+                context.globalAlpha = particle.life / ParticleEngine.particleTypes[particle.type].maxLife;
+
+                const image = ParticleEngine.particleTypes[particle.type].image.image;
+                context.drawImage(image, particle.position[0] - image.width / 2, particle.position[1] - image.height / 2);
+            }
         }
+
+        context.restore();
     }
 }
 
