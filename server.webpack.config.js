0fd1652cba63bbd0250d4a0e69956cbce7d56728
Karl Hobley <karlhobley10@gmail.com>
2015-12-30 20:14:19 +0000

    Removed unneeded webpack files

--- server.webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    context: __dirname + "/build",
-    entry: "./server",
-    output: {
-        path: __dirname + "/dist",
-        filename: "rs-server-[hash].js"
-    }
-}
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    context: __dirname + "/build",
-    entry: "./rippyshreddy",
-    output: {
-        path: __dirname + "/dist",
-        filename: "rs-[hash].js"
-    }
-}
